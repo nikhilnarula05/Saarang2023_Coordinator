fd3aa4f37b3d3f49bdd0498fa09792fcd5501e58
nikhilnarula05 <nikhilnarula05@gmail.com>
2022-07-20 09:52:03 +0530

    Changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,7 @@
 import React from "react";
-import './header.css';
-//import logoUrl from '../../assets/logo.svg';
 import logo from './logo.jpg'
 function Header() {
     return(
-        // <nav class="bg-dark navbar-dark navbar">
-        //     <div className="row col-12 d-flex justify-content-center text-white">
-        //         <h3>Saarang Registration</h3>
-        //     </div>
-        // </nav>
     <div className="header">
     <img 
     src={logo} 
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,3 +1,5 @@
+import Iframe from 'react-iframe'
+
 function Header(props) {
     return(
           <div className="welcomePage">
@@ -9,7 +11,16 @@ function Header(props) {
           src="https://images.pexels.com/photos/65006/pexels-photo-65006.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="Welcome Cat"
           ></img>
+          <Iframe 
+          style="border-radius:12px" 
+          src="https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b?utm_source=generator" 
+          width="100%" 
+          height="80" 
+          frameBorder="0" 
+          allowfullscreen="" 
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"/>
           </div>
     )
 }
 export default Header;
+
